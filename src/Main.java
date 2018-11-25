4a73f4ee44a8f8b08dcd138f55b0b2189aa9d85c
Ryuji Ogawa <e185736@ie.u-ryukyu.ac.jp>
2018-11-26 01:32:02 +0900

    report4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args){
-        String str = null;
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
+
+
 
         try {
             System.out.print(str.length());
